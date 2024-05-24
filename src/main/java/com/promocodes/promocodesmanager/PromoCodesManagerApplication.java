6b457079c676669ffb5bb25ecd5c1c7dad8e8da6
Alek <aleksander.dul.77@gmail.com>
2024-05-24 17:17:41 +0200

    init project config

---
+++ src/main/java/com/promocodes/promocodesmanager/PromoCodesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.promocodes.promocodesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoCodesManagerApplication.class, args);
+	}
+
+}
