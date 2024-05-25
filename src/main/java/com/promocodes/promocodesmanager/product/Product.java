76f0fc540afeaf2634a1701683991333512769a4
Alek <aleksander.dul.77@gmail.com>
2024-05-25 14:50:01 +0200

    impl 'product' entity

---
+++ src/main/java/com/promocodes/promocodesmanager/product/Product.java
@@ -0,0 +1,122 @@
+package com.promocodes.promocodesmanager.product;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @SequenceGenerator(
+            name = "products_sequence",
+            sequenceName = "products_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "products_sequence"
+    )
+    @Column(name="product_id")
+    Long productId;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private String currency;
+
+    public Product() {}
+
+    public Product(String name,
+                   String description,
+                   Double price,
+                   String currency) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public Product(Long productId,
+                   String name,
+                   String description,
+                   Double price,
+                   String currency) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId)
+                && Objects.equals(name, product.name)
+                && Objects.equals(description, product.description)
+                && Objects.equals(price, product.price)
+                && Objects.equals(currency, product.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, name, description, price, currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
