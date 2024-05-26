beb63d60251fa1e95275f4bc778990207201bebc
Alek <aleksander.dul.77@gmail.com>
2024-05-27 00:00:29 +0200

    impl exception handler of 'FailedToAddNewProductException' class

---
+++ src/main/java/com/promocodes/promocodesmanager/product/ProductExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.promocodes.promocodesmanager.product;
+
+import com.promocodes.promocodesmanager.exception.ApiExceptionDto;
+import com.promocodes.promocodesmanager.exception.ExceptionMapper;
+import com.promocodes.promocodesmanager.exception.FailedToAddNewProductException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProductExceptionHandler {
+    private final ExceptionMapper exceptionMapper;
+
+    @Autowired
+    public ProductExceptionHandler(ExceptionMapper exceptionMapper) {
+        this.exceptionMapper = exceptionMapper;
+    }
+
+    @ExceptionHandler({FailedToAddNewProductException.class})
+    public ResponseEntity<ApiExceptionDto> handleFailedToAddNewProductException(FailedToAddNewProductException ex) {
+        ApiExceptionDto exceptionDto = exceptionMapper.toApiExceptionDto(ex);
+        HttpStatus httpStatus = ex.getStatus();
+
+        return new ResponseEntity<>(exceptionDto, httpStatus);
+    }
+}
