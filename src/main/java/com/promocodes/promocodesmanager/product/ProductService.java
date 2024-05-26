5979233d272ca8cb3a3acb3491736b457924e389
Alek <aleksander.dul.77@gmail.com>
2024-05-26 16:28:15 +0200

    impl 'addNewProduct' method in product service class

--- src/main/java/com/promocodes/promocodesmanager/product/ProductService.java
+++ src/main/java/com/promocodes/promocodesmanager/product/ProductService.java
@@ -1,6 +1,14 @@
 package com.promocodes.promocodesmanager.product;
 
+import com.promocodes.promocodesmanager.exception.FailedToAddNewProductException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
 import java.util.List;
 
 @Service
@@ -13,5 +21,86 @@ public class ProductService {
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @Transactional
+    public Product addNewProduct(String name,
+                                 String description,
+                                 Double price,
+                                 String currency) {
+        if (name == null || name.isEmpty()) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    "Name of new product can not be null or empty"
+            );
+        }
+
+        if (productRepository.existsByName(name)) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("'%s' product name is taken", name)
+            );
+        }
+
+        if (price == null) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    "Price of new product can not be null or empty"
+            );
+        }
+
+        if (price < 0) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    "Price of new product can not be less than zero"
+            );
+        }
+
+        if (currency == null || currency.isEmpty()) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    "Currency of new product can not be null or empty"
+            );
+        }
+
+        if (!isValidCurrency(currency)) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("Currency '%s' is invalid", currency)
+            );
+        }
+
+        try {
+            String formatedName = StringUtils.capitalize(name);
+            String formatedDescription = StringUtils.capitalize(description);
+            Double formatedPrice = BigDecimal.valueOf(price)
+                    .setScale(2, RoundingMode.DOWN)
+                    .doubleValue();
+            String formattedCurrency = currency.toUpperCase();
+
+            Product product = new Product();
+            product.setName(formatedName);
+            product.setDescription(formatedDescription);
+            product.setPrice(formatedPrice);
+            product.setCurrency(formattedCurrency);
+
+            return productRepository.save(product);
+        } catch (Exception ex) {
+            throw new FailedToAddNewProductException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Unknown error occurred"
+            );
+        }
+    }
+
+    private boolean isValidCurrency(String currency) {
+        try {
+            String formattedCurrency = currency.toUpperCase();
+
+            return Currency.getAvailableCurrencies()
+                    .contains(Currency.getInstance(formattedCurrency));
+        } catch (Exception ex) {
+            return false;
+        }
     }
 }
--- src/test/java/com/promocodes/promocodesmanager/product/ProductServiceTest.java
+++ src/test/java/com/promocodes/promocodesmanager/product/ProductServiceTest.java
@@ -1,10 +1,15 @@
 package com.promocodes.promocodesmanager.product;
 
+import com.promocodes.promocodesmanager.exception.FailedToAddNewProductException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,5 +29,268 @@ class ProductServiceTest {
 
         verify(productRepository, times(1))
                 .findAll();
+    }
+
+    @Test
+    public void shouldAddNewProduct() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "USD";
+
+        productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        );
+
+        ArgumentCaptor<Product> productArgumentCaptor =
+                ArgumentCaptor.forClass(Product.class);
+
+        verify(productRepository, times(1))
+                .save(productArgumentCaptor.capture());
+
+        assertThat(productArgumentCaptor.getValue().getName())
+                .isEqualTo(name);
+        assertThat(productArgumentCaptor.getValue().getDescription())
+                .isEqualTo(description);
+        assertThat(productArgumentCaptor.getValue().getPrice())
+                .isEqualTo(price);
+        assertThat(productArgumentCaptor.getValue().getCurrency())
+                .isEqualTo(currency);
+    }
+
+    @Test
+    public void shouldFormatAndAddNewProduct() {
+        String name = "apple";
+        String description = "sample fruit";
+        Double price = 1.128390;
+        String currency = "usd";
+
+        productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        );
+
+        ArgumentCaptor<Product> productArgumentCaptor =
+                ArgumentCaptor.forClass(Product.class);
+
+        verify(productRepository, times(1))
+                .save(productArgumentCaptor.capture());
+
+        assertThat(productArgumentCaptor.getValue().getName())
+                .isEqualTo("Apple");
+        assertThat(productArgumentCaptor.getValue().getDescription())
+                .isEqualTo("Sample fruit");
+        assertThat(productArgumentCaptor.getValue().getPrice())
+                .isEqualTo(1.12);
+        assertThat(productArgumentCaptor.getValue().getCurrency())
+                .isEqualTo("USD");
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfNameIsNullable() {
+        String name = null;
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "USD";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Name of new product can not be null or empty"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfNameIsEmpty() {
+        String name = "";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "USD";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Name of new product can not be null or empty"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfProductOfGivenNameAlreadyExists() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "USD";
+
+        when(productRepository.existsByName(anyString()))
+                .thenReturn(true);
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        String.format("'%s' product name is taken", name)
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfPriceIsNullable() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = null;
+        String currency = "USD";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Price of new product can not be null or empty"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfPriceIsLessThanZero() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = -1.12;
+        String currency = "USD";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Price of new product can not be less than zero"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfCurrencyIsNullable() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = null;
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Currency of new product can not be null or empty"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfCurrencyIsEmpty() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        "Currency of new product can not be null or empty"
+                );
+    }
+
+    @Test
+    public void shouldThrowFailedToAddNewProductExceptionIfCurrencyIsInvalid() {
+        String name = "Apple";
+        String description = "Fruit";
+        Double price = 1.12;
+        String currency = "EEE";
+
+        assertThatThrownBy(() -> productService.addNewProduct(
+                name,
+                description,
+                price,
+                currency
+        )).isInstanceOf(FailedToAddNewProductException.class)
+                .extracting(
+                        "message",
+                        "status",
+                        "debugMessage"
+                )
+                .containsExactly(
+                        "Failed to add new product.",
+                        HttpStatus.BAD_REQUEST,
+                        String.format("Currency '%s' is invalid", currency)
+                );
     }
 }
