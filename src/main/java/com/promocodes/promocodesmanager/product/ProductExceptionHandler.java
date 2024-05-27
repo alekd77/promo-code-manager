4096d6d13181c7f1202349960d98ffc8dfa3a287
Alek <aleksander.dul.77@gmail.com>
2024-05-27 17:07:20 +0200

    impl exception handler of 'HttpMessageNotReadableException' in product exception handler

--- src/main/java/com/promocodes/promocodesmanager/product/ProductExceptionHandler.java
+++ src/main/java/com/promocodes/promocodesmanager/product/ProductExceptionHandler.java
@@ -7,8 +7,11 @@ import com.promocodes.promocodesmanager.exception.FailedToUpdateProductException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ProductExceptionHandler {
@@ -17,6 +20,19 @@ public class ProductExceptionHandler {
     @Autowired
     public ProductExceptionHandler(ExceptionMapper exceptionMapper) {
         this.exceptionMapper = exceptionMapper;
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity<ApiExceptionDto> handleNumberFormatException(HttpMessageNotReadableException ex) {
+        ApiExceptionDto exceptionDto = new ApiExceptionDto(
+                "Invalid input data format.",
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage()
+        );
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        return new ResponseEntity<>(exceptionDto, httpStatus);
     }
 
     @ExceptionHandler({FailedToAddNewProductException.class})
