79ddbeedd8815c16694726ebfebc05fdfab4af54
Alek <aleksander.dul.77@gmail.com>
2024-05-27 03:53:25 +0200

    added '@Autowired' annotation to product service class costructor

--- src/main/java/com/promocodes/promocodesmanager/product/ProductService.java
+++ src/main/java/com/promocodes/promocodesmanager/product/ProductService.java
@@ -2,6 +2,7 @@ package com.promocodes.promocodesmanager.product;
 
 import com.promocodes.promocodesmanager.exception.FailedToAddNewProductException;
 import com.promocodes.promocodesmanager.exception.FailedToUpdateProductException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import java.util.Optional;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
