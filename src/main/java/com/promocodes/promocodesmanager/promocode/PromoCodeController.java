a39cb38d678c8b46fc795d1550d6423d36da0905
Alek <aleksander.dul.77@gmail.com>
2024-05-27 17:13:18 +0200

    impl 'addNewFixedAmountPromoCode' endpoint in promo code controller class

--- src/main/java/com/promocodes/promocodesmanager/promocode/PromoCodeController.java
+++ src/main/java/com/promocodes/promocodesmanager/promocode/PromoCodeController.java
@@ -6,9 +6,7 @@ import com.promocodes.promocodesmanager.product.ProductResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,18 @@ public class PromoCodeController {
         List<PromoCodeResponseDto> promoCodeResponseDtoList = promoCodeMapper.toPromoCodesResponseDtoList(promoCodes);
 
         return new ResponseEntity<>(promoCodeResponseDtoList, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/fixed-amount")
+    public ResponseEntity<String> addNewFixedAmountPromoCode(@RequestBody FixedAmountPromoCodeDto dto) {
+        promoCodeService.addNewFixedAmountPromoCode(
+                dto.getText(),
+                dto.getExpirationDate(),
+                dto.getUsagesAllowed(),
+                dto.getDiscountAmount(),
+                dto.getDiscountCurrency()
+        );
+
+        return new ResponseEntity<>("Promo code has been successfully added.", HttpStatus.CREATED);
     }
 }
