199c59df0860702b19dca402006d5c08cabeaf0c
Alek <aleksander.dul.77@gmail.com>
2024-06-04 01:10:04 +0200

    added more tests for purchase repository

--- src/test/java/com/promocodes/promocodesmanager/purchase/PurchaseRepositoryTest.java
+++ src/test/java/com/promocodes/promocodesmanager/purchase/PurchaseRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.promocodes.promocodesmanager.purchase;
 
-import com.promocodes.promocodesmanager.product.Product;
 import com.promocodes.promocodesmanager.product.ProductRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -8,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class PurchaseRepositoryTest {
@@ -48,6 +49,68 @@ class PurchaseRepositoryTest {
                 .usingRecursiveComparison()
                 .ignoringFields("purchaseId")
                 .isEqualTo(purchase);
+    }
+
+    @Test
+    public void shouldFindAllPurchases() {
+        Purchase purchase1 = new Purchase(
+                null,
+                "NikeShoes",
+                LocalDate.now().minusDays(5),
+                155.55,
+                100.0,
+                "USD"
+        );
+
+        Purchase purchase2 = new Purchase(
+                null,
+                "NikeShoes",
+                LocalDate.now().minusDays(5),
+                155.55,
+                80.0,
+                "USD"
+        );
+
+        Purchase purchase3 = new Purchase(
+                null,
+                "NikeShoes",
+                LocalDate.now().minusDays(5),
+                155.55,
+                70.0,
+                "USD"
+        );
+
+        List<Purchase> savedPurchases = new ArrayList<>();
+        savedPurchases.add(
+                purchaseRepository.save(purchase1)
+        );
+        savedPurchases.add(
+                purchaseRepository.save(purchase2)
+        );
+        savedPurchases.add(
+                purchaseRepository.save(purchase3)
+        );
+
+        List<Purchase> foundPurchases = purchaseRepository.findAll();
+
+        assertThat(foundPurchases)
+                .isNotNull();
+        assertThat(savedPurchases.size())
+                .isEqualTo(foundPurchases.size());
+        assertThat(savedPurchases)
+                .containsExactlyInAnyOrderElementsOf(foundPurchases);
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfNoPurchaseIsFound() {
+        List<Purchase> foundPurchases = purchaseRepository.findAll();
+
+        assertThat(foundPurchases)
+               .isNotNull();
+        assertThat(foundPurchases.size())
+               .isEqualTo(0);
+        assertThat(foundPurchases)
+                .isEqualTo(Collections.emptyList());
     }
 
 }
